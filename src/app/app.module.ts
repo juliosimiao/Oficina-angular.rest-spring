f8049525f0d3d8492864b4bad2e5f3647f76dac8
julioSimiao <julio-eddie-616@hotmail.com>
2018-02-23 11:46:40 -0300

    Criação do serviço que integra o webService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { DataTableModule } from 'primeng/primeng';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ItemCadastroComponent } from './item-cadastro/item-cadastro.component';
+import { ItemService } from './item.service';
 
 
 @NgModule({
@@ -16,9 +18,12 @@ import { ItemCadastroComponent } from './item-cadastro/item-cadastro.component';
   ],
   imports: [
     BrowserModule,
-    DataTableModule
+    DataTableModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ItemService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
